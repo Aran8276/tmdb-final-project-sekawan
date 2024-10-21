03d4b004b67ddd73606bb2441f13ba3592435555
Aran8276 <zahranzn61@gmail.com>
2024-10-21 23:24:15 +0700

    Fancy animations

--- src/components/MovieCarousel.tsx
+++ src/components/MovieCarousel.tsx
@@ -8,10 +8,20 @@ import {
   CarouselNext,
 } from "./ui/carousel";
 import { Search } from "@/types/Search";
+import { delay, motion } from "framer-motion";
 
 interface SelfProps {
   data: Search;
 }
+
+const variants = {
+  hidden: { opacity: 0, y: -400 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, ease: "easeInOut" },
+  },
+};
 
 export default function MovieCarousel(props: SelfProps) {
   return (
@@ -19,15 +29,23 @@ export default function MovieCarousel(props: SelfProps) {
       <CarouselContent className="w-[350px]">
         {props.data?.results.map((item, index) => {
           return (
-            <CarouselItem className="">
-              <MovieCover
-                key={index}
-                to={"/movie/" + item.id}
-                title={item.title ? item.title : ""}
-                description={item.overview}
-                img={imgBaseUrlPoster + item.poster_path}
-              />
-            </CarouselItem>
+            <motion.div
+              initial="hidden"
+              viewport={{ once: true }}
+              whileInView="visible"
+              variants={variants}
+              className="px-4"
+            >
+              <CarouselItem className="">
+                <MovieCover
+                  key={index}
+                  to={"/movie/" + item.id}
+                  title={item.title ? item.title : ""}
+                  description={item.overview}
+                  img={imgBaseUrlPoster + item.poster_path}
+                />
+              </CarouselItem>
+            </motion.div>
           );
         })}
       </CarouselContent>
